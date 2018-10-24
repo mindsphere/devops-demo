29e7b618b38b5e31c086262f98ecb463c6fea7ee
Diego Louzán <diego.louzan@gmail.com>
2018-10-24 10:20:44 +0200

    refactor(todo): refine login redirect and extra csp config
    - refine login redirect when session expires, invalidate xsrf cookie
      so that cors preflight options request works properly at the
      mdsp level
    - document extra csp settings required for login redirects when user
      session token expires

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -3,7 +3,7 @@ Copyright Siemens AG 2018
 SPDX-License-Identifier: MIT
 */
 
-import {Inject, Injectable} from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import {
   HttpEvent,
   HttpInterceptor,
@@ -11,8 +11,8 @@ import {
   HttpRequest, HttpErrorResponse
 } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { EMPTY, Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { DOCUMENT } from '@angular/common';
 
 @Injectable()
@@ -21,12 +21,24 @@ export class AuthInterceptor implements HttpInterceptor {
 
   intercept (req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
-      tap((err: any) => {
+      catchError((err, caught) => {
         // If we detect a 401 Unauthorized on any request (including xhr),
         // redirect to the login page managed by the MindSphere Gateway
-        if (err instanceof HttpErrorResponse && err.status === 401) {
+        // This can also happen on failed CORS requests, and Angular sees
+        // status code 0
+        //
+        // Also remove the xsrf-token cookie before redirecting, or the
+        // preflight OPTIONS request triggered by the browser via CORS will fail
+        // at mdsp due to the extra added x-xsrf-token header added by angular
+        if (err instanceof HttpErrorResponse && [0, 401].includes(err.status)) {
+          console.log('Detected potential CORS or authentication issue');
+          console.log('Invalidating XSRF-TOKEN cookie and redirecting to /login');
+
+          this.document.cookie = 'XSRF-TOKEN=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
           this.document.location.href = '/login';
+          return EMPTY;
         }
+        return throwError(err);
       })
     );
   }
